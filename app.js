c5d8d049adf5340be549c69b715d17e01b02d2c7
unknown <jacovancr@gmail.com>
2018-08-10 14:55:40 +0100

    removed bad port config

--- app.js
+++ app.js
@@ -14,9 +14,6 @@ var experienceRouter = require('./routes/experience')
 
 var app = express();
 
-app.set('port', process.env.PORT || 8080);
-app.listen(app.get('port'));
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
