57b427812f72c8758b6261b0b85a05895c1d387d
unknown <jacovancr@gmail.com>
2018-11-24 16:05:27 +0000

    Design changes and some content

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index')
+var indexRouter = require('./routes/index');
 var homeRouter = require('./routes/home');
 var contactRouter = require('./routes/contact');
 var projectsRouter = require('./routes/projects');
 var educationRouter = require('./routes/education');
-var experienceRouter = require('./routes/experience')
-var solitaireRouter = require('./routes/solitaire')
+var experienceRouter = require('./routes/experience');
+var solitaireRouter = require('./routes/solitaire');
 
 
 var app = express();
